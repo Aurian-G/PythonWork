2b485a39f5187c1a2310b5282361e91b56fb308b
Aurian-G <47487534+Aurian-G@users.noreply.github.com>
2019-05-21 16:43:28 -0500

    Add files via upload
    Guessing Game

---
+++ Guessing_Game.py
@@ -0,0 +1,46 @@
+import random
+
+import time
+
+num = random.randint(1,100)
+guesses = [0]
+
+print('''WELCOME TO GUESS ME!\n
+I'm thinking of a number between 1 and 100
+If your guess is more than 10 away from my number, I'll tell you you're COLD
+If your guess is within 10 of my number, I'll tell you you're WARM
+If your guess is farther than your most recent guess, I'll say you're getting \nCOLDER
+If your guess is closer than your most recent guess, I'll say you're getting \nWARMER
+LET'S PLAY!''')
+
+while True:
+
+    # create input for the user to guess
+    guess = int(input("I'm thinking of a number between 1 and 100. \nWhat is your guess? "))
+    
+    if guess < 1 or guess > 100:
+        print('OUT OF BOUNDS! Please try again: ')
+        continue
+    
+    # here we compare the player's guess to our number
+    if guess == num:
+        print(f'CONGRATULATIONS, YOU GUESSED IT IN ONLY {len(guesses)} GUESSES!!')
+        break
+        
+    # if guess is incorrect, add guess to the list
+    guesses.append(guess)
+    
+    # when testing the first guess, guesses[-2]==0, which evaluates to False
+    # and brings us down to the second section
+    
+    if guesses[-2]:  
+        if abs(num-guess) < abs(num-guesses[-2]):
+            print('WARMER!')
+        else:
+            print('COLDER!')
+   
+    else:
+        if abs(num-guess) <= 10:
+            print('WARM!')
+        else:
+            print('COLD!')
